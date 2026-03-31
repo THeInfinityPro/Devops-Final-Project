28283152d9087a3afce435ecc42146c9c42edee1
root <root@ip-172-31-38-152.ec2.internal>
2026-03-31 23:27:47 +0000

    deploy.sh file fix

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,12 @@ BRANCH=${1:-dev}  # default to dev
 DEV_REPO="jagadishhak/dev:$BRANCH"
 PROD_REPO="jagadishhak/prod:$BRANCH"
 
-
-
 docker-compose down || true #stop previously existing containers
-
 
 if [ "$BRANCH" == "dev" ]; then
     sed -i "s|image: .*|image: $DEV_REPO|" docker-compose.yml
 elif [ "$BRANCH" == "master" ]; then
     sed -i "s|image: .*|image: $PROD_REPO|" docker-compose.yml
 fi
-
 
 docker-compose up -d
